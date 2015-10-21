1cb6c1370fb51a110123b13aa74b7565d482402d
Selina Brinnich <sbrinnich@student.tgm.ac.at>
2015-10-21 07:49:26 +0200

    Changed everything AGAIN!

--- src/dsn/static/js/management.js
+++ src/dsn/static/js/management.js
@@ -1,115 +1,20 @@
-var managementApp = angular.module('managementApp', ['ui.router']);
+var mainApp = angular.module('mainApp');
 
-managementApp.config(function($stateProvider, $urlRouterProvider, $locationProvider, $httpProvider) {
-
-    $urlRouterProvider.otherwise('/management');
-
-    // TIMETABLE
-    $stateProvider.state('timetable', {
-        url: '/management',
-        templateUrl: '/management/management_timetable.html',
-        controller: 'timetableCtrl'
-    });
-
-    // ACCOUNT SETTINGS
-    $stateProvider.state('accsettings', {
-        url: '/management',
-        templateUrl: '/management/management_accsettings.html',
-        controller: 'accsettingsCtrl'
-    });
-
-    // NOTEBOOKS
-    $stateProvider.state('notebooks', {
-        url: '/management/notebooks',
-        templateUrl: '/management/management_notebooks.html',
-        controller: 'notebooksCtrl'
-    });
-
-    // NOTEBOOKS
-    $stateProvider.state('notebooks_create', {
-        url: '/management/notebooks_create',
-        templateUrl: '/management/management_notebooks_create.html',
-        controller: 'notebooksCtrl_create'
-    });
-
-    // PROFILE
-    $stateProvider.state('profile', {
-        url: '/management/profile',
-        templateUrl: '/management/management_profile.html',
-        controller: 'profileCtrl'
-    });
-
-    $locationProvider.html5Mode(true);
-
-    // CSRF TOKEN
-    $httpProvider.defaults.xsrfCookieName = 'csrftoken';
-    $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
+mainApp.controller('managementCtrl', function(){
 
 });
 
-managementApp.controller('accsettingsCtrl', function($scope){
+mainApp.controller('accsettingsCtrl', function($scope){
     $scope.a = '1';
 });
 
-managementApp.controller('notebooksCtrl', function($scope){
+mainApp.controller('notebooksCtrl', function($scope){
     $scope.a = '2';
 });
 
-managementApp.controller('timetableCtrl', function($scope, $window, loggedIn){
-    $scope.a = '3';
-    if(!loggedIn.isAdmin()){
-        $window.location.href = '/';
-    }
-});
+mainApp.controller('timetableCtrl', function($scope, $window, $rootScope, loggedIn){
 
-managementApp.service('loggedIn', function ($http) {
-    this.isAdmin = function () {
-        var user = null;
-        $http({
-            method  : 'POST',
-            url     : '/api/loggedInUser',
-            headers : {'Content-Type': 'application/json'},
-            data    : {}
-        })
-            .success(function(data){
-                alert("Success");
-                user = data['user'];
-                if(user == null){
-                    return false;
-                }else{
-                    return user.is_admin;
-                }
-            })
-            .error(function(data){
-                alert("Error");
-            });
-
-    }
-})
-
-managementApp.controller('notebooksCtrl_create', function($scope){
-    var name = $scope.name;
-    var public = $scope.public;
-
-    $scope.submitCreateNotebook = function() {
-        if (!$scope.error) {
-            $http({
-                method: 'POST',
-                url: '/api/notebooks_create',
-                headers: {'Content-Type': 'application/json'},
-                data: {
-                    name: name,
-                    public: public
-                }
-
-            });
-        }
-    }
-});
-
-managementApp.controller('timetableCtrl', function($scope,$http){
     $scope.submitTimeTable= function(){
-        alert("hh");
         var gegenstand = $scope.gegenstand;
         var lehrer = $scope.lehrer;
         var anfangszeit = $scope.anfang;
@@ -129,26 +34,44 @@ managementApp.controller('timetableCtrl', function($scope,$http){
     }
 });
 
-managementApp.controller('logoutCtrl', function($scope, $http){
+mainApp.controller('notebooksCreateCtrl', function($scope){
+    var name = $scope.name;
+    var public = $scope.public;
+
+    $scope.submitCreateNotebook = function() {
+        if (!$scope.error) {
+            $http({
+                method: 'POST',
+                url: '/api/notebooks_create',
+                headers: {'Content-Type': 'application/json'},
+                data: {
+                    name: name,
+                    public: public
+                }
+
+            });
+        }
+    }
+});
+
+mainApp.controller('logoutCtrl', function($scope, $http, $state){
     $scope.logout = function(){
+        alert("Test");
         $http({
             method  : 'GET',
             url     : '/api/logout',
             data    : {}
         })
             .success(function(data){
-                if (data['logout_error'] != null) {
-                    $scope.error = true;
-                    $scope.logout_error = data['logout_error'];
-                }
+                $state.go('mainpage.content');
             })
             .error(function(data){
-
+                alert('Beim ausloggen ist ein Fehler aufgetreten! Bitte versuche es erneut!');
             });
     }
 });
 
-managementApp.controller('profileCtrl', function($scope, $http){
+mainApp.controller('profileCtrl', function($scope, $http){
     $http({
         method: 'GET',
         url: '/api/profile'
