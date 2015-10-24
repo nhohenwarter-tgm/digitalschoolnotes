a6efe549b255576d2c6285f0e3b1d4759586e040
Selina Brinnich <sbrinnich@student.tgm.ac.at>
2015-10-24 15:56:02 +0200

    Added Testing project

---
+++ test/src/Parameters.java
@@ -0,0 +1,56 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;;
+import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.concurrent.TimeUnit;
+
+public class Parameters{
+    public static final int FIREFOX = 0;
+    public static final int INTERNET_EXPLORER = 1;
+    public static final int CHROME = 2;
+    public static final int SAFARI = 3;
+
+    public static final String LINUX = "linux";
+    public static final String MAC = "mac";
+    public static final String WINDOWS = "windows.exe";
+
+    public static String os = LINUX; //HIER OS AENDERN
+    public static int browser = FIREFOX; //HIER BROWSER AENDERN
+    public static int port = 5001; //HIER PORT AENDERN
+
+    public static String baseUrl = "http://digitalschoolnotes.com:"+port;
+
+    public static WebDriver driver;
+
+    public static void setUpBrowser() throws Exception {
+        switch(browser){
+            case FIREFOX:
+                driver = new FirefoxDriver();
+                break;
+            case INTERNET_EXPLORER:
+                if(os == LINUX || os == MAC){
+                    System.err.println("Falsches OS du honk!");
+                    System.exit(0);
+                }
+                System.setProperty("webdriver.ie.driver", "webdrivers/iedriver_"+os);
+                DesiredCapabilities caps = DesiredCapabilities.internetExplorer(); caps.setCapability(InternetExplorerDriver.INITIAL_BROWSER_URL, "");
+                driver = new InternetExplorerDriver(caps);
+                break;
+            case CHROME:
+                System.setProperty("webdriver.chrome.driver", "webdrivers/chromedriver_"+os);
+                driver = new ChromeDriver();
+                break;
+            case SAFARI:
+                if(os == LINUX){
+                    System.err.println("Falsches OS du honk!");
+                    System.exit(0);
+                }
+                driver = new SafariDriver();
+                break;
+        }
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    }
+}
