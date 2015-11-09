75186013d4ba265fefd6dc9154393b4607763f90
Niklas Hohenwarter <info@niklashohenwarter.com>
2015-11-09 22:16:18 +0100

    everything today

--- src/dsn/urls.py
+++ src/dsn/urls.py
@@ -41,7 +41,7 @@ urlpatterns = [
     url(r'^api/get_notebooks', 'dsn.views.management_views.view_get_notebooks', name="getNotebooks"),
     url(r'^api/get_notebook', 'dsn.views.management_views.view_get_notebook', name="getNotebook"),
     url(r'^api/get_userSettings', 'dsn.views.management_views.view_getUserSettings', name="getUserSettings"),
-    url(r'^api/user_edit', 'dsn.views.management_views.view_editUser', name="setUser")
+    url(r'^api/user_edit', 'dsn.views.management_views.view_editUser', name="setUser"),
     url(r'^api/oauth/google/request', 'dsn.views.mainpage_views.view_google_oauth_request', name="googleOauthRequest"),
     url(r'^api/oauth/google/response', 'dsn.views.mainpage_views.view_google_oauth_response', name="googleOauthResponse"),
 ]
