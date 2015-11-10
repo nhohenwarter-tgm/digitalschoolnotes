db8b5566921cadb9f108c85c01277cef4723c3e5
Niklas Hohenwarter <info@niklashohenwarter.com>
2015-11-10 11:39:23 +0100

    seems like this plugin wont work, have to use an other one

--- src/dsn/authentication/oauth.py
+++ src/dsn/authentication/oauth.py
@@ -7,21 +7,29 @@ from django.conf import settings
 google_authorization_base_url = 'https://accounts.google.com/o/oauth2/auth'
 google_token_url = 'https://accounts.google.com/o/oauth2/token'
 
+
 def oauth_google_request(request):
     scope = ['https://www.googleapis.com/auth/userinfo.email',
              'https://www.googleapis.com/auth/userinfo.profile']
-    print(request.META['SERVER_PORT'])
-    baseurl = "https://digitalschoolnotes.com:"+str(int(request.META['SERVER_PORT'])-3000)
-    google = OAuth2Session(settings.OAUTH_GOOGLE_PUB, scope=scope, redirect_uri = baseurl+"/api/oauth/google/response")
+    google = OAuth2Session(settings.OAUTH_GOOGLE_PUB, scope=scope, redirect_uri = build_request_uri(request)+"/api/oauth/google/response")
     authorization_url, state = google.authorization_url(google_authorization_base_url)
     request.session['oauth_state']=state
 
     return redirect(authorization_url)
 
 def oauth_google_callback(request):
+    build_response_uri(request)
     google = OAuth2Session(settings.OAUTH_GOOGLE_PUB, state=request.session['oauth_state'])
-    print(request.build_absolute_uri())
     token = google.fetch_token(google_token_url, client_secret=settings.OAUTH_GOOGLE_PRIV,
-                               authorization_response=request.build_absolute_uri())
+                               authorization_code=build_response_uri(request))
 
     return JsonResponse(google.get('https://api.github.com/user').json())
+
+def build_response_uri(request):
+    code = request.GET.get('code')
+    state = request.GET.get('state')
+    baseurl = "https://digitalschoolnotes.com:"+str(int(request.META['SERVER_PORT'])-3000) + "/api/oauth/google/response"
+    baseurl += "?state="+state+"&code="+code+"#"
+
+def build_request_uri(request):
+    return "https://digitalschoolnotes.com:"+str(int(request.META['SERVER_PORT'])-3000)
