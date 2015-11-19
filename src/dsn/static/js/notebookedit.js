0744d8db8f02d4786c8a28db5d5b027ec14cb157
Philipp Adler <padler@student.tgm.ac.at>
2015-11-19 02:31:38 +0100

    Merge

--- src/dsn/static/js/notebookedit.js
+++ src/dsn/static/js/notebookedit.js
@@ -1,64 +1,63 @@
 var mainApp = angular.module('mainApp');
 
 mainApp.controller('notebookEditCtrl', function ($scope, $http, $stateParams, $sce, $window, loggedIn) {
-mainApp.controller('notebookEditCtrl', function($scope, $http, $stateParams, $sce, $window, loggedIn){
 
-    $scope.code=function(){
+    $scope.code = function () {
         //$scope.thisCanBeusedInsideNgBindHtml += "<html>hhhhh</html>";
         //Methode1
         /**
-        var myEl = angular.element( document.querySelector( '#test' ) );
-myEl.append('<section > <textarea rows="6" cols="70" ui-codemirror="cmOption"></textarea> Mode : <select ng-model="mode" ng-options="m for m in modes" ng-change="modeChanged()"></select> </section>');
+         var myEl = angular.element( document.querySelector( '#test' ) );
+         myEl.append('<section > <textarea rows="6" cols="70" ui-codemirror="cmOption"></textarea> Mode : <select ng-model="mode" ng-options="m for m in modes" ng-change="modeChanged()"></select> </section>');
 
 
-        //Methode2
-        var ni = document.getElementById('myDiv');
+         //Methode2
+         var ni = document.getElementById('myDiv');
 
-  var numi = document.getElementById('theValue');
+         var numi = document.getElementById('theValue');
 
-  var num = (document.getElementById('theValue').value -1)+ 2;
+         var num = (document.getElementById('theValue').value -1)+ 2;
 
-  numi.value = num;
+         numi.value = num;
 
-  var newdiv = document.createElement('div');
+         var newdiv = document.createElement('div');
 
-  var divIdName = 'my'+num+'Div';
+         var divIdName = 'my'+num+'Div';
 
-  newdiv.setAttribute('id',divIdName);
+         newdiv.setAttribute('id',divIdName);
 
-  newdiv.innerHTML = '<section > <textarea rows="6" cols="70" ui-codemirror="cmOption"></textarea> Mode : <select ng-model="mode" ng-options="m for m in modes" ng-change="modeChanged()"></select> </section>';
+         newdiv.innerHTML = '<section > <textarea rows="6" cols="70" ui-codemirror="cmOption"></textarea> Mode : <select ng-model="mode" ng-options="m for m in modes" ng-change="modeChanged()"></select> </section>';
 
-  ni.appendChild(newdiv); **/
+         ni.appendChild(newdiv); **/
 
-        //Methode3 --> funktioniert
+            //Methode3 --> funktioniert
         alert("hhh");
-         $scope.divHtmlVar = $scope.divHtmlVar + '<section> <textarea class="codestyle" rows="6" cols="70" ui-codemirror="cmOption"></textarea> Mode : <select ng-model="mode" ng-options="m for m in modes" ng-change="modeChanged()"></select> </section>';
+        $scope.divHtmlVar = $scope.divHtmlVar + '<section> <textarea class="codestyle" rows="6" cols="70" ui-codemirror="cmOption"></textarea> Mode : <select ng-model="mode" ng-options="m for m in modes" ng-change="modeChanged()"></select> </section>';
     };
     // The modes
-  $scope.modes = ['Scheme', 'XML', 'Javascript','clike','python'];
-  $scope.mode = $scope.modes[0];
+    $scope.modes = ['Scheme', 'XML', 'Javascript', 'clike', 'python'];
+    $scope.mode = $scope.modes[0];
 
 
-  // The ui-codemirror option
-  $scope.cmOption = {
-      lineNumbers: true,
-      indentWithTabs: true,
-      onLoad: function (_cm) {
+    // The ui-codemirror option
+    $scope.cmOption = {
+        lineNumbers: true,
+        indentWithTabs: true,
+        onLoad: function (_cm) {
 
-          // HACK to have the codemirror instance in the scope...
-          $scope.modeChanged = function () {
-              _cm.setOption("mode", $scope.mode.toLowerCase());
-          };
-      }
-  };
+            // HACK to have the codemirror instance in the scope...
+            $scope.modeChanged = function () {
+                _cm.setOption("mode", $scope.mode.toLowerCase());
+            };
+        }
+    };
 
-       /**Initial code content...
-  $scope.cmModel = ';; Scheme code in here.\n' +
-    '(define (double x)\n\t(* x x))\n\n\n' +
-    '<!-- XML code in here. -->\n' +
-    '<root>\n\t<foo>\n\t</foo>\n\t<bar/>\n</root>\n\n\n' +
-    '// Javascript code in here.\n' +
-    'function foo(msg) {\n\tvar r = Math.random();\n\treturn "" + r + " : " + msg;\n}'; **/
+    /**Initial code content...
+     $scope.cmModel = ';; Scheme code in here.\n' +
+     '(define (double x)\n\t(* x x))\n\n\n' +
+     '<!-- XML code in here. -->\n' +
+     '<root>\n\t<foo>\n\t</foo>\n\t<bar/>\n</root>\n\n\n' +
+     '// Javascript code in here.\n' +
+     'function foo(msg) {\n\tvar r = Math.random();\n\treturn "" + r + " : " + msg;\n}'; **/
 
     $scope.publicViewed = true;
     $scope.xPos = {};
@@ -80,15 +79,6 @@ myEl.append('<section > <textarea rows="6" cols="70" ui-codemirror="cmOption"></
             } else {
                 $scope.publicViewed = true;
             }
-    }).success(function(data){
-        $scope.notebook = JSON.parse(data['notebook']);
-        loggedIn.getUser().then(function(data){
-            var user = data['user'];
-            if($scope.notebook['email'] == user['email']){
-                $scope.publicViewed = false;
-            }else{
-                $scope.publicViewed = true;
-            }
 
             angular.element('#book').booklet({
                 startingPage: $scope.notebook['numpages'] - 1,
@@ -103,19 +93,6 @@ myEl.append('<section > <textarea rows="6" cols="70" ui-codemirror="cmOption"></
                 pagePadding: 0
             });
             $scope.currentPage = $scope.notebook['numpages'] - 1;
-            angular.element('#book').booklet({
-                startingPage: $scope.notebook['numpages']-1,
-                next: '#goto-next',
-                prev: '#goto-prev',
-                easing:  null,
-                easeIn:  null,
-                easeOut: null,
-                shadows: false,
-                width: "1100",
-                height: "700",
-                pagePadding: 0
-            });
-            $scope.currentPage = $scope.notebook['numpages']-1;
 
             angular.element('#goto-start').click(function (e) {
                 e.preventDefault();
@@ -125,24 +102,11 @@ myEl.append('<section > <textarea rows="6" cols="70" ui-codemirror="cmOption"></
                 e.preventDefault();
                 angular.element('#book').booklet("gotopage", "end");
             });
-            angular.element('#goto-start').click(function(e){
-                e.preventDefault();
-                angular.element('#book').booklet("gotopage", "start");
-            });
-            angular.element('#goto-end').click(function(e){
-                e.preventDefault();
-                angular.element('#book').booklet("gotopage", "end");
-            });
 
             $scope.makeDraggable('testy', 1);
             $scope.makeDraggable('testy2', 1);
-            $scope.makeDraggable('testy',1);
-            $scope.makeDraggable('testy2',1);
 
         }, function (data) {
-            $scope.notebook = JSON.parse(data['notebook']);
-            $scope.publicViewed = true;
-        }, function(data){
             $scope.notebook = JSON.parse(data['notebook']);
             $scope.publicViewed = true;
 
@@ -150,12 +114,6 @@ myEl.append('<section > <textarea rows="6" cols="70" ui-codemirror="cmOption"></
                 width: "",
                 height: "",
                 startingPage: $scope.notebook['numpages'] - 1,
-                next: '#goto-next',
-                prev: '#goto-prev'
-            $('#book').booklet({
-                width: "",
-                height: "",
-                startingPage: $scope.notebook['numpages']-1,
                 next: '#goto-next',
                 prev: '#goto-prev'
             });
@@ -199,18 +157,18 @@ myEl.append('<section > <textarea rows="6" cols="70" ui-codemirror="cmOption"></
     $scope.createElementReference = function () {
         var input = $window.prompt("Auf welche Seite möchtest du referenzieren?", "");
         if (input != null) {
-    $scope.createElementReference = function(){
-        var input = $window.prompt("Auf welche Seite m�chtest du referenzieren?","");
-        if(input != null) {
-            $scope.pages[1] = $scope.pages[1] + '<div id="reference_' + $scope.count['reference'] + '" ' +
-                'style="position: absolute;"><em>' +
-                '<a ng-click="toPage(' + input + ')">Siehe Seite ' + input + '</a></em></div>';
-            $scope.makeDraggable('reference_' + $scope.count['reference'], 1);
-            $scope.count['reference'] = $scope.count['reference'] + 1;
+            $scope.createElementReference = function () {
+                var input = $window.prompt("Auf welche Seite m�chtest du referenzieren?", "");
+                if (input != null) {
+                    $scope.pages[1] = $scope.pages[1] + '<div id="reference_' + $scope.count['reference'] + '" ' +
+                        'style="position: absolute;"><em>' +
+                        '<a ng-click="toPage(' + input + ')">Siehe Seite ' + input + '</a></em></div>';
+                    $scope.makeDraggable('reference_' + $scope.count['reference'], 1);
+                    $scope.count['reference'] = $scope.count['reference'] + 1;
+                }
+            };
         }
     };
-
-});
 
     $scope.deleteelement = function (id, art) {
         $http({
@@ -243,20 +201,9 @@ myEl.append('<section > <textarea rows="6" cols="70" ui-codemirror="cmOption"></
         this.hoverEdit = false;
     };
 
-})
-;
+});
 
 mainApp.directive('compile', ['$compile', function ($compile) {
-    return function(scope, element, attrs) {
-        scope.$watch(
-            function(scope) {
-                // watch the 'compile' expression for changes
-                return scope.$eval(attrs.compile);
-            },
-            function(value) {
-                // when the 'compile' expression changes
-                // assign it into the current DOM
-                element.html(value);
     return function (scope, element, attrs) {
         scope.$watch(
             function (scope) {
@@ -267,18 +214,31 @@ mainApp.directive('compile', ['$compile', function ($compile) {
                 // when the 'compile' expression changes
                 // assign it into the current DOM
                 element.html(value);
+                return function (scope, element, attrs) {
+                    scope.$watch(
+                        function (scope) {
+                            // watch the 'compile' expression for changes
+                            return scope.$eval(attrs.compile);
+                        },
+                        function (value) {
+                            // when the 'compile' expression changes
+                            // assign it into the current DOM
+                            element.html(value);
 
-                // compile the new DOM and link it to the current
-                // scope.
-                // NOTE: we only compile .childNodes so that
-                // we don't get into infinite loop compiling ourselves
-                $compile(element.contents())(scope);
+                            // compile the new DOM and link it to the current
+                            // scope.
+                            // NOTE: we only compile .childNodes so that
+                            // we don't get into infinite loop compiling ourselves
+                            $compile(element.contents())(scope);
+                        }
+                    );
+                };
             }
-        );
-    };
+        )
+    }
 }]);
 angular.element('app').app();
-$scope.ckEditor = function() {
+$scope.ckEditor = function () {
     return {
         require: '?ngModel',
         link: function ($scope, elm, attr, ngModel) {
@@ -296,13 +256,3 @@ $scope.ckEditor = function() {
         }
     }
 };
-
-                // compile the new DOM and link it to the current
-                // scope.
-                // NOTE: we only compile .childNodes so that
-                // we don't get into infinite loop compiling ourselves
-                $compile(element.contents())(scope);
-            }
-        );
-    };
-}]);
