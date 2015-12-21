91ed542e81fd0e0aa48d98b681a5aaf9deca918a
Niklas Hohenwarter <info@niklashohenwarter.com>
2015-12-21 15:17:29 +0100

    language now can be set

--- src/dsn/static/js/notebookedit.js
+++ src/dsn/static/js/notebookedit.js
@@ -264,6 +264,10 @@ mainApp.controller('notebookEditCtrl', function ($scope, $http, $stateParams, $s
     // CODE ELEMENT
     $scope.codeLanguage="";
 
+    $scope.setCodeElementLanguage = function(lang){
+        $scope.codeLanguage = lang;
+    }
+
     $scope.addCodeElement = function(){
         data = "{\"data\":\"\", \"language\":\""+$scope.codeLanguage+"\"}";
         console.log(data);
