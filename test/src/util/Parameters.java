e81aefc936f8ffe91c54049920fff8ad2088a8f0
Niklas Hohenwarter <info@niklashohenwarter.com>
2016-02-08 15:27:46 +0100

    tests betweenstatus

--- test/src/util/Parameters.java
+++ test/src/util/Parameters.java
@@ -21,12 +21,12 @@ public class Parameters{
 
     public static String os = LINUX; //HIER OS AENDERN
     public static int browser = FIREFOX; //HIER BROWSER AENDERN
-    public static int port = 5004; //HIER PORT AENDERN
+    public static int port = 5001; //HIER PORT AENDERN
 
     public static boolean stable = false;
 
     //E-Mail Adresse, bei der die E-Mails tatsächlich abgerufen werden können (muss existierender Benutzer in DSN sein!)
-    public static String testingEmail = "sbrinnich@gmx.at"; //HIER EMAIL AENDERN
+    public static String testingEmail = "info@niklashohenwarter.com"; //HIER EMAIL AENDERN
     //Testing Passwort muss zwischen 8 und 128 Zeichen haben und sollte anders sein als das
     // aktuelle Passwort des Users mit der obigen E-Mail Adresse
     public static String testingPwd = "12345678"; //HIER TEST-PASSWORT AENDERN
